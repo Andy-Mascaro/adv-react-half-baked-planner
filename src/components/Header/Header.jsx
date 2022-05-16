8d8b322f2a9b5661a72737c60ccd79602fc49fd5
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-16 15:20:40 -0700

    added entries to entrylist from context and fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import Entry from '../Planner/EntryItem';
+import { useEntries } from '../../context/PlannerContext';
 import styles from './Header.css';
 
+
 export default function Header() {
+  const { entries } = useEntries();
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
       {/* TODO: Replace hard-coded count with the actual number of entries */}
-      <span>Number of items in planner:{Entry.length}</span>
+      <span>Number of items in planner:{entries.length}</span>
     </header>
   );
 }
